5f8f48e7065687559c70d814f52dd9e2bd255dcf
CherylHenkels <cherylhenkelss@gmail.com>
2024-06-07 21:12:37 -0300

    [M2S04] Ex 2 - Filtragem de Elementos

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let numeros = [1,2,3,4,5,6];
 let soma = numeros.reduce((total, num) => total + num, 0)
 
 console.log(soma);
+
+// Exercicio 2
+let pares = numeros.filter(n=> {return n%2 == 0})
+
+console.log(pares);
